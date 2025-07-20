48b3a0a970514615c939872d15cbd8edad9c0b70
joaquingalang <galangjoaquin@gmail.com>
2025-07-20 20:23:23 +0800

    Update: fetch quotes from api ninja

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+
+  const apiKey = process.env.REACT_APP_API_NINJAS_KEY;
+
+  async function fetchQuote() {
+  setIsLoading(true);
+  try {
+    const apiUrl = "https://api.api-ninjas.com/v1/quotes";
+    let response = await fetch(apiUrl, {
+      method: "GET",
+      headers: {
+        "X-Api-Key": apiKey,
+      },
+    });
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    const data = await response.json();
+    setQuote(data[0].quote);
+    setAuthor(data[0].author);
+    setIsLoading(false);
+  } catch (e) {
+    console.error(e.message);
+  }
+}
+
+  useEffect(() => {
+    fetchQuote();
+  }, []);
+
+  let content = (
+    <div className="content">
+          <h1 className="quote">{`"${quote}"`}</h1>
+          <h3 className="author">
+            <em>{`- ${author}`}</em>
+          </h3>
+        </div>
+  );
+
+  let loader = (
+    <div className="loader"></div>
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="background"></div>
+
+      <div className="container">
+
+        {isLoading ? loader : content}
+        
+        <button className="quote-btn" onClick={() => fetchQuote()}>New Quote</button>
+      </div>
+    </>
   );
 }
 
